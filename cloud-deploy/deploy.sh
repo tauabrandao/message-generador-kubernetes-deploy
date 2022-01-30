e7648b97cf9cc5c5397263832759b967b27d9625
Tauã Brandão <brandao.taua@gmail.com>
2022-01-30 16:32:55 -0300

    Update deploy.sh

--- cloud-deploy/deploy.sh
+++ cloud-deploy/deploy.sh
@@ -8,7 +8,3 @@ echo 'waiting 10 seconds to deploy the API'
 
 sleep 10
 kubectl create -f api/
-
-echo
-echo 'API URL below - it may take a few seconds for you to be able to access it.'
-minikube service api-service --url
